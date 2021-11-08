118e213428f7875b7e3e55c563ae942db92623a4
richarda0538 <richarda0538@gmail.com>
2021-11-08 15:47:53 +0530

    ml bootcamp

---
+++ main.py
@@ -0,0 +1,5 @@
+n=input()
+if n[::-1] == n:
+    print("palindrome")
+else:
+    print("Not palindrome")
---
+++ main1.py
@@ -0,0 +1,7 @@
+n=int(input())
+for i in range(n):
+    if(i*i == n):
+        print("perfect square")
+        break
+else:
+    print("not Perfect Square")

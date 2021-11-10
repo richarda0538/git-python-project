1b4de8440cdd56cc3bca4ae77512b292b207d52a
richarda0538 <richarda0538@gmail.com>
2021-11-10 10:32:28 +0530

    print statement was updated

--- main1.py
+++ main1.py
@@ -1,7 +1,7 @@
 n=int(input())
 for i in range(n):
     if(i*i == n):
-        print("perfect square")
+        print("Yes, given no. is perfect square")
         break
 else:
     print("not Perfect Square")
